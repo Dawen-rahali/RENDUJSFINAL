ae5ab083aca721498c0c19abe72dc520e2b28ebb
Dawen-rahali <DAWEN.RAHALIMARTIN@latoile.em-lyon.com>
2025-05-21 17:25:34 +0200

    commit

--- script.js
+++ script.js
@@ -3,45 +3,60 @@ function Contenu() {
     fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/menuiserie.json")
         .then(response => response.json())
         .then(data => {
-          console.log(data);
+             console.log(data);
 
           const div = document.getElementById("premierediv");
+
+
           div.innerHTML = `
           <h1>${data.entreprise}</h1>
-          <p>${data.propositionDeValeur}</p>
+          <p id="proposition">${data.propositionDeValeur}</p>
+          <div class="boutondiv">
           <button>${data.texteBouton}</button>
-          <p>${data.promessesClients}</p>
-            `
+          </div>
+            `;
 
+             data.promessesClients.forEach(element => {
+             div.innerHTML+= `
+                <ul>
+                    <li>${element}</li>
+                </ul>
+                  `              
+             })
 
           const section = document.getElementById("masection");
-          section.innerHTML = `
-          <div>
-          <h1>${data.titre}</h1>
-          <p>${data.description}</p>
-          <img src="${data.image-url}</img>
-          <div>
+            data.realisations.forEach(element => {
+                section.innerHTML+=`
+                <div>
+                    <h3>${element.titre}</h3>
+                    <p>${element.description}</p>
+                    <img src="${element['image-url']}">
+                </div>
             `
-          });
-            
+            })
 
-
-
+            const deuxiemediv = document.getElementById("deuxiemediv");
+                data.temoignages.forEach(element => {
+                    deuxiemediv.innerHTML+=`
+                    <div>
+                        <h4>${element.prenom}</h4>
+                        <h5>${element.typePrestation}</h5>
+                        <blockquote>${element.commentaire}</blockquote>
+                        <p>${element.note}</p>
+                    </div>
+                `;
+                })
         
+        })
+
+
+
+
 
 
 
     
-        
+
+
 }
-
-
-Contenu();
-
-
-
-
-
-
-
-    
+    Contenu();
